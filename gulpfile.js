cc2b1156a79ae6a157ddac5dcbfe98dd67ec0d37
Nicholas Bertolani <nicholas.bertolani@copiaincolla.com>
2023-04-28 15:43:27 +0200

    aggiunta cose

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,66 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
-var imagemin = require ('gulp-imagemin');
+'use strict';
 
-// TASK LIST
+/**
+ * LOAD PLUGIN
+ * external plugin into node-modules
+ */
+var autoprefixer        = require('gulp-autoprefixer'),
+    changed             = require('gulp-changed'),
+    cleanCSS            = require('gulp-clean-css'),
+    gulp                = require('gulp'),
+    imagemin            = require('gulp-imagemin'),
+    sass                = require('gulp-sass');
+
+/**
+ * GULP SASS
+ */
 function gulpSass() {
-    return gulp.src('./assets/sass/**/*.scss')
+    return gulp.src('./assets/sass/**/*.{scss,css}')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/assets/css/'))
+        .pipe(cleanCSS({rebase: false}))
+        .pipe(autoprefixer({
+            remove: false
+        }))
+        .pipe(gulp.dest('./public/assets/css/'));
 }
 
-function gulpImage() {
+/**
+ * IMAGES
+ */
+function images() {
     return gulp.src('./assets/img/**/*.{png,gif,jpg,jpeg}')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./public/assets/img/'))
+        .pipe(changed('./public/assets/img/'))
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({
+                interlaced: true,
+                optimizationLevel: 7
+            })
+        ], {
+            verbose: true
+        }))
+        .pipe(gulp.dest('./public/assets/img/'));
 }
 
+/**
+ * WATCH
+ */
 function watchFiles() {
-    gulp.watch('./assets/sass/**/*.scss', gulpSass);
-    gulp.watch('./assets/img/**/*.{png,gif,jpg,jpeg}', gulpImage);
+    gulp.watch('./assets/img/**/*.{png,gif,jpg,jpeg}', images);
+    gulp.watch('./assets/sass/**/*', gulpSass);
 }
 
-
-
-// EXPORT TASK
+/**
+ * EXPORT
+ */
+exports.images = images;
 exports.gulpSass = gulpSass;
+exports.watchFiles = watchFiles;
 
-exports.gulpImage = gulpImage;
+var build = gulp.series(gulpSass, gulp.parallel(images));
+var watch = gulp.parallel(watchFiles);
 
-var build = gulp.series(gulpSass);
-
-gulp.task('images', gulpImage);
-
-gulp.task('watch', watchFiles);
-
+gulp.task('build', build);
+gulp.task('watch', watch);
 gulp.task('default', build);
